da36a56dc815bcbf3829b69ea6657d8f87140fbe
theoaked <eduardo.sc@protonmail.com>
2020-09-27 23:04:27 -0300

    fix anima

--- game/World/Grass.gd
+++ game/World/Grass.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
-func _proccess(delta):
+func _process(delta):
 	if Input.is_action_just_pressed("attack"):
 		var GrassEffect = load("res://Effects/GrassEffect.tscn")
 		var grassEffect = GrassEffect.instance()
-		var world = get_tree().current_scence
+		var world = get_tree().get_current_scene()
+		world.add_child(grassEffect)
 		grassEffect.global_position = global_position
 		queue_free()
